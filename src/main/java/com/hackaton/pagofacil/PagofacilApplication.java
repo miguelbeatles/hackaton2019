e9d65fb5790dfc2714a463cfe07ff75b509fa396
sphint <sphint_avs@hotmail.com>
2019-08-24 14:33:33 -0500

    init

---
+++ src/main/java/com/hackaton/pagofacil/PagofacilApplication.java
@@ -0,0 +1,13 @@
+package com.hackaton.pagofacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagofacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagofacilApplication.class, args);
+	}
+
+}
